f37494846ace4d26a9c0bede30571e429ab36c3d
yuta <yutatakeuchi3721@gmail.com>
2019-02-10 01:32:29 +0900

    ・ファイルの保存先をExternal/publicに変更 ・ギャラリーに登録されない

--- app/src/main/java/com/example/android/sharingfiles/FileSelectActivity.java
+++ app/src/main/java/com/example/android/sharingfiles/FileSelectActivity.java
@@ -2,8 +2,10 @@ package com.example.android.sharingfiles;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
@@ -14,6 +16,7 @@ import android.widget.ImageView;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileSelectActivity extends AppCompatActivity {
     private Intent intent = new Intent("com.example.android.sharingfiles.ACTION_RETURN_FILE");
@@ -28,11 +31,12 @@ public class FileSelectActivity extends AppCompatActivity {
         imageView.setImageURI(imageUri);
 
         //Create file
-        File imagePath = new File(getExternalFilesDir(null),"shareimages");
+        File imagePath = Environment.getExternalStoragePublicDirectory(Environment
+                .DIRECTORY_PICTURES);
         if(!imagePath.isDirectory()){
             imagePath.mkdirs();
         }
-        File newfile = new File(imagePath,"1mage_1.jpg");
+        File newfile = new File(imagePath,"image_1.jpg");
         FileOutputStream fos;
         try{
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
@@ -46,6 +50,30 @@ public class FileSelectActivity extends AppCompatActivity {
 
         Uri fileUri = FileProvider.getUriForFile(this,"com.example.android.sharingfiles" +
                 ".fileprovider",newfile);
+        //MediaStoreへの登録
+        addMediaStoreByMediaScanner(newfile);
+
+        //addMediaStoreByIntentの確認　ここから
+
+        //Rawから共有ストレージへ保存
+        //Uriの生成
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.image_2);
+        //共有ストレージへ保存
+        File newFile2 = new File(imagePath,"image_2.jpg");
+        FileOutputStream fos2;
+        try{
+            //Bitmapの生成
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            fos2 = new FileOutputStream(newFile2);
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos2);
+            fos2.close();
+        }catch (IOException e){
+
+        }
+        //MediaStoreへの登録
+        addMediaStoreByIntent(newFile2);
+
+        //addMediaStoreByIntentの確認　ここまで
 
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.setDataAndType(fileUri,getContentResolver().getType(fileUri));
@@ -59,4 +87,28 @@ public class FileSelectActivity extends AppCompatActivity {
     public void select(View view){
         finish();
     }
+
+    //MediaStoreへのファイル登録(1)
+    private void addMediaStoreByMediaScanner(File file){
+        MediaScannerConnection.scanFile(this, new String[]{file.getAbsolutePath()}, null, new
+                MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.d("addMediaStoreByScanner",path+" is added to MediaStore");
+                    }
+                });
+
+    }
+
+    //MediaStoreへのファイル登録(2)
+    private void addMediaStoreByIntent(File file){
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        this.sendBroadcast(intent);
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/android/sharingfiles/MainActivity.java
+++ app/src/main/java/com/example/android/sharingfiles/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.sharingfiles;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,30 +25,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(Intent.createChooser(intent,"Pick From:"),0);
+
+        //パーミッションの確認
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission
+                .WRITE_EXTERNAL_STORAGE);
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE},0);
+        }
+
+        Button btn_FileSelect = (Button)findViewById(R.id.btn_fileSelect);
+        btn_FileSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Pick From:"),0);
+            }
+        });
+        Button btn_Document = (Button) findViewById(R.id.btn_document);
+        btn_Document.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent,1);
+
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent returnIntent){
-        if(requestCode == 0 && resultCode == RESULT_OK){
-            ImageView iv = (ImageView)findViewById(R.id.image);
+        if(requestCode == 0 && resultCode == RESULT_OK) {
+            ImageView iv = (ImageView) findViewById(R.id.image);
 
             Uri resultUri = returnIntent.getData();
             iv.setImageURI(resultUri);
             ParcelFileDescriptor mPFD = null;
             try {
-                mPFD = getContentResolver().openFileDescriptor(resultUri,"r");
+                mPFD = getContentResolver().openFileDescriptor(resultUri, "r");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
             FileDescriptor descriptor = mPFD.getFileDescriptor();
 
-            Cursor returnCursor = getContentResolver().query(resultUri,null,null,null,null);
-            TextView titleText = (TextView)findViewById(R.id.title);
-            TextView sizeText = (TextView)findViewById(R.id.size);
-            TextView typeText  = (TextView) findViewById(R.id.type);
+            Cursor returnCursor = getContentResolver().query(resultUri, null, null, null, null);
+            TextView titleText = (TextView) findViewById(R.id.title);
+            TextView sizeText = (TextView) findViewById(R.id.size);
+            TextView typeText = (TextView) findViewById(R.id.type);
 
             int titleIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
             int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
@@ -50,7 +82,11 @@ public class MainActivity extends AppCompatActivity {
             titleText.setText(returnCursor.getString(titleIndex));
             sizeText.setText(Long.toString(returnCursor.getLong(sizeIndex)));
             typeText.setText(getContentResolver().getType(resultUri));
-
+        }
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Uri resultUri = returnIntent.getData();
+            ImageView iv = (ImageView)findViewById(R.id.image);
+            iv.setImageURI(resultUri);
         }
     }
 }
